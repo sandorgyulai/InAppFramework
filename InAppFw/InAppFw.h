075f089782166e6df29d844f3ba0bd0d6ca9663e
SaÃÅndor Gyulai <sandor.gyulai@pontsystems.eu>
2015-10-12 16:19:17 +0200

    Initial Commit

---
+++ InAppFw/InAppFw.h
@@ -0,0 +1,19 @@
+//
+//  InAppFw.h
+//  InAppFw
+//
+//  Created by Sándor Gyulai on 12/10/15.
+//  Copyright © 2015 Sándor Gyulai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppFw.
+FOUNDATION_EXPORT double InAppFwVersionNumber;
+
+//! Project version string for InAppFw.
+FOUNDATION_EXPORT const unsigned char InAppFwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppFw/PublicHeader.h>
+
+
